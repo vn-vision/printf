36a2107c7a250e7afa74a326f1e8a54ca26283b8
muchira007 <muchirasteve2@gmail.com>
2023-11-12 23:38:49 -0800

    debugging for formatspec

--- formatspec.c
+++ formatspec.c
@@ -1,17 +1,27 @@
 #include <main.h>
+/**
+ * _printf -  creates a function that looks for format specifiers
+ * returns number of char printed excluding
+ *  : null byte '\0'
+ * @format:  string output
+ * write in output in standardoutput format
+ * @printme:
+ * @no_char:
+ * Return: no of characters
+ */
 
 int _printf(const char *format, ...)
 {
 	va_list printme;
 	int no_char = 0;
 
-	va_start(printme, format );
+	va_start(printme, format);
 
-	while(*format != '\0')
+	while (*format != '\0')
 	{
 		if (*format == '%')
 		{
-			format ++;
+			format++;
 			switch (*format)
 			{
 				case 'd':
@@ -20,26 +30,30 @@ int _printf(const char *format, ...)
 				int num = va_arg(args, int);
 				char buffer[12];
 				int len = sprintf(buffer, "%d", num);
+
 				printme += write(1, buffer, len);
 				}
 			break;
 			default:
 
-			return(-1);
+			return (-1);
 		}
 	}
-	else 
+	else
 	{
 		printme += write(1, format, 1);
 	}
 	format++;
 }
 	va_end(printme);
-	return no_char;
+	return (no_char);
 	}
-
-int main()
+/**
+ * main - prints the output
+ * Return: 0
+ */
+int main(void)
 {
 	_printf("Integer: %d \n", 42);
-	return 0;
+	return (0);
 }
--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _rintf(const char format, ...);
 void check_modifier(va_list nArgs, const char **format, unsigned int *count);
+va_list printme;
+int no_char = 0;
+int main (void)
+
 #endif
