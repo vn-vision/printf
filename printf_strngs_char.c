6bfe17d1d0c7cdec12f3413c46689623753d1d55
muchira007 <muchirasteve2@gmail.com>
2023-11-13 23:44:17 -0800

    segmentation fault

--- printf_strngs_char.c
+++ printf_strngs_char.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include <string.h>
 
 /**
 * _print - produces output according to format
@@ -10,73 +7,57 @@
 * write output to stdout stream
 * @format: character string
 * @nArgs: stores variable arguments
+* char c for char
 * @count: keeps count of characters
 * Return: 0 Always true.
 *
 */
-
 void check_modifier(va_list nArgs, const char *format, int *count);
-
 int _print(const char *format, ...)
 {
-	int count = 0, i = 0;
+	int count = 0;
 	va_list nArgs;
 
-	/**
-	* check whether format is empty
-	*/
 	if (format == NULL)
 		return (-1);
 
 	va_start(nArgs, format);
 
-	/**
-	* check whether index points to null byte
-	* loop until null byte is reached
-	*/
 	while (*format)
 	{
-		/**
-		* check if index points to %
-		* if so check the next char
-		* if not print the char
-		*/
-		if (format[i] != '%')
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
 		{
 			format++;
-			if (format[i] == '\0')
+			if (*format == '\0')
 				break;
 
-			/**
-			* char c for char
-			*/
-			if (format[i] == 'c')
+			if (*format == 'c')
 			{
-				char c = va_arg(nArgs, int);
+				int c = va_arg(nArgs, int);
 
 				write(1, &c, 1);
 				count++;
 			}
-			else if (format[i] == '%')
+			else if (*format == '%')
 			{
 				write(1, format, 1);
 				count++;
 			}
-			else if (format[i] == 's')
+			else if (*format == 's')
 			{
 				char *str = va_arg(nArgs, char *);
 
-				if (str != NULL && *str != '\0')
+				if (str != NULL)
 				{
 					write(1, str, strlen(str));
 					count += strlen(str);
 				}
 			}
-		}
-		else
-		{
-			write(1, format, 1);
-			count++;
 		}
 		format++;
 	}
